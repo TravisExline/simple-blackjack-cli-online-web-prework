62eef6488866e026debb3505b2053a00ad0ced80
Travis Exline <t.exline22@gmail.com>
2019-04-29 17:18:43 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,8 +24,7 @@ def end_game(card_total)
 end
 
 def initial_round(your_cards)
-  deal_card
-  deal_card
+  your_cards = deal_card deal_card
   display_card_total
   puts "#{display_card_total}"# code #initial_round here
 end
